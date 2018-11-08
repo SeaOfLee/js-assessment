ed58175f5158e3795331cc0c76249e64d504eb25
SeaOfLee <l.richardson1@gmail.com>
2018-11-08 15:57:42 -0800

    update growl & jquery.

--- app/functions.js
+++ app/functions.js
@@ -21,11 +21,24 @@ exports.functionsAnswers = {
     });
   },
 
-  partial: function(fn, str1, str2) {},
+  partial: function(fn, str1, str2) {
+    return function(str3) {
+      return fn(str1, str2, str3);
+    };
+  },
 
-  useArguments: function() {},
+  useArguments: function() {
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+    return sum;
+  },
 
-  callIt: function(fn) {},
+  callIt: function(fn) {
+    var args = Array.prototype.slice(arguments);
+    return fn.call(this, args);
+  },
 
   partialUsingArguments: function(fn) {},
 
