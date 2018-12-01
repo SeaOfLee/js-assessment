56564dd94c203588ad825782b828c64c5d98797c
SeaOfLee <l.richardson1@gmail.com>
2018-11-30 21:04:48 -0800

    add regexp solutions

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,36 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    var re = /\d/;
+    return re.test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    var re = /([a-zA-Z]).*?\1/;
+    return re.test(str);
   },
 
   endsWithVowel: function(str) {
-
+    var re = /[aeiouAEIOU]$/;
+    return re.test(str);
   },
 
   captureThreeNumbers: function(str) {
-
+    var re = /(\d{3})/;
+    var result = re.exec(str);
+    if (result) {
+      return result[0];
+    }
+    return false;
   },
 
   matchesPattern: function(str) {
-
+    var re = /^(\d{3}-\d{3}-\d{4})$/;
+    return re.test(str);
   },
 
   isUSD: function(str) {
-
+    var re = /^(\$\d{1,3})(,\d{3})*(\.\d{2})?$/;
+    return re.test(str);
   }
 };
