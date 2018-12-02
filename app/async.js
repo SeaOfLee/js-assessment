2bd802c2cde29f11a11ba2e6dd4937e5bfbca8aa
SeaOfLee <l.richardson1@gmail.com>
2018-12-01 21:33:21 -0800

    solve async

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(function(res, rej) {
+      res(value);
+    });
   },
 
   manipulateRemoteData: function(url) {
+    var names = fetch(url)
+      .then(function(response) {
+        var clone = response.clone();
+        return clone.json();
+      })
+      .then(function(apiRes) {
+        names = apiRes.people
+          .map(function(person) {
+            return person.name;
+          })
+          .sort();
+        return names;
+      });
 
+    return names;
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    var result = '';
+    var dic = {};
+    for (var char of str) {
+      console.log(char);
+      if (dic[char]) {
+        dic[char]++;
+      } else {
+        dic[char] = 1;
+      }
+    }
+    console.log(dic);
   },
 
-  wordWrap: function(str, cols) {
+  wordWrap: function(str, cols) {},
 
-  },
-
-  reverseString: function(str) {
-
-  }
+  reverseString: function(str) {}
 };
