9eb93b0ca010afcb57ca6977cc21c31c61162ed2
Lee Richardson <lee@getvictorious.com>
2018-12-04 15:16:30 -0800

    add fibonacci

--- app/count.js
+++ app/count.js
@@ -2,16 +2,12 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function(start, end) {
-    (function() {
-      var current = start;
-      setInterval(function() {
-        console.log(current);
-        if (current < end) {
-          console.log('nope');
-          current += 1;
-        }
-      }, 100);
-    });
-    //
+    var current = start;
+    function updateCurrent(current) {
+      return current++;
+    }
+    setInterval(function() {
+      updateCurrent(current);
+    }, 100);
   }
 };
--- app/recursion.js
+++ app/recursion.js
@@ -22,7 +22,13 @@ exports.recursionAnswers = {
 
   permute: function(arr) {},
 
-  fibonacci: function(n) {},
+  fibonacci: function(n) {
+    if (n <= 2) {
+      return 1;
+    }
+
+    return this.fibonacci(n - 1) + this.fibonacci(n - 2);
+  },
 
   validParentheses: function(n) {}
 };
